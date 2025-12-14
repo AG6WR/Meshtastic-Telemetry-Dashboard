19128ea541d985ffd90dc690a33b44ec8ddc2235
Brian Whitaker <brian.whitaker@mac.com>
2025-12-14 12:43:12 -0800

    Version 1.2.1b - UI improvements: kiosk-ready fonts, button sizing, window positioning

--- dashboard.py
+++ dashboard.py
@@ -623,7 +623,7 @@ class SettingsDialog:
         self.dialog.destroy()
 
 # Version number - update manually with each release
-VERSION = "1.2.0b"
+VERSION = "1.2.1b"
 
 def get_version_info():
     """Get version information"""
